7ae57a5394cd1b146fbae0de28200522797017f7
Raulpassos08 <raulfera2@gmail.com>
2024-09-17 11:19:59 -0300

    Compartilhamento de dados, componente pai com o componente filho

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  username = 'Joaquim'
+  userData = {
+    email : 'joaquim@gmail.com'
+  }
+
   title = 'ProjectNew';
 }
